8d33d64e5b78b0a12cc68000ede56a68e134e5de
Wouter van Atteveldt <wva@societal-analytics.labs.vu.nl>
2026-05-20 16:21:42 +0200

    Load main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from contextlib import asynccontextmanager
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
